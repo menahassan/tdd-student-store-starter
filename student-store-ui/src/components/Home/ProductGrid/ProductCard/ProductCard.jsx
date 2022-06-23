fe7256125f8214dcefb33c37a18400b9484faf21
menahassan <menah9@gmail.com>
2022-06-23 10:31:17 -0700

    add product detail view

--- student-store-ui/src/components/Home/ProductGrid/ProductCard/ProductCard.jsx
+++ student-store-ui/src/components/Home/ProductGrid/ProductCard/ProductCard.jsx
@@ -8,10 +8,27 @@ export default function ProductCard({product, productId, quantity, handleAddItem
         description = <p className="product-description">{product.description}</p>
     }
     return (
-        <div className="product-card">
+        <div>
             {
                     showDescription
                     ?
+                    <div className="large-product-card">
+                    <div>
+                        <Link className = "media" to={`/products/${productId}`}>
+                            <img class = "large-product-img" src={product.image} alt={`${product.name} image`} />
+                        </Link>
+                        <p className="product-name">{product.name}</p>
+                        <p className="product-price">{`$${product.price.toFixed(2)}`}</p>
+                        <p className="product-quantity">{`Quantity: ${quantity}`}</p>
+                        <div>
+                            <button className="add button" onClick={handleAddItemToCart}>Add</button>
+                            <button className="remove button" onClick={handleRemoveItemToCart}>Remove</button>
+                        </div>
+                        {description}
+                    </div>
+                    </div>
+                    : 
+                    <div className="product-card">
                     <div>
                         <Link className = "media" to={`/products/${productId}`}>
                             <img class = "product-img" src={product.image} alt={`${product.name} image`} />
@@ -25,19 +42,6 @@ export default function ProductCard({product, productId, quantity, handleAddItem
                         </div>
                         {description}
                     </div>
-                    : 
-                    <div>
-                        <Link className = "media" to={`/products/${productId}`}>
-                            <img class = "product-img" src={product.image} alt={`${product.name} image`} />
-                        </Link>
-                        <p className="product-name">{product.name}</p>
-                        <p className="product-price">{`$${product.price.toFixed(2)}`}</p>
-                        <p className="product-quantity">{`Quantity: ${quantity}`}</p>
-                        <div>
-                            <button className="add button" onClick={handleAddItemToCart}>Add</button>
-                            <button className="remove button" onClick={handleRemoveItemToCart}>Remove</button>
-                        </div>
-                        {description}
                     </div>
             }
          
